9d3a9f5ef47e52f35bdbad52194560fd48c1ef53
Marcus Wörner <61889355+woernerm@users.noreply.github.com>
2023-12-27 21:44:14 +0100

    Add js tests

--- js_library/src/localematch.js
+++ js_library/src/localematch.js
@@ -3,7 +3,7 @@ import {match} from '@formatjs/intl-localematcher'
 export function localematch(desired, supported, fallback){
     let matches = [];
     for(let i=0; i < Math.min(supported.length, desired.length); i++){
-        let e = match(desired, supported.slice(i), fallback, {algorithm: 'best fit'});
+        let e = match(desired[i], supported, fallback, {algorithm: 'best fit'});
         if (!matches.includes(e)){matches.push(e);}
     }
     return matches;
--- js_library/tests/localematch.test.js
+++ js_library/tests/localematch.test.js
@@ -1,6 +1,30 @@
-import { expect, test } from 'vitest'
+import { expect, test, describe } from 'vitest'
 import { localematch } from '../src/localematch.js'
 
-test('Return_the_best_matching_locale_for_a_single_requested_one', () => {
-  expect(localematch(["en-US"], ["en-US", "en-GB"], "en-US")).toEqual(["en-US"])
+describe('TestJSMatch', () => {
+    test('return_the_best_matching_locale_for_a_single_requested_one', () => {
+        const available = ["en-US", "en-GB"];
+        expect(localematch(["en-US"], available, "en-US")).toEqual(["en-US"]);
+        expect(localematch(["en-GB"], available, "en-US")).toEqual(["en-GB"]);
+        expect(localematch(["en-CA"], available, "en-US")).toEqual(["en-GB"]);
+    })
+
+    test('return_requested_locale_if_it_is_part_of_the_available_ones', () => {
+        const available = ["en-US", "en-GB", "en-CA"];
+        expect(localematch(["en-US"], available, "en-US")).toEqual(["en-US"]);
+        expect(localematch(["en-GB"], available, "en-US")).toEqual(["en-GB"]);
+        expect(localematch(["en-CA"], available, "en-US")).toEqual(["en-CA"]);
+    })
+
+    test('return_a_list_of_best_matching_locales_for_multiple_requested_ones', () => {
+        const available = ["en-US", "en-GB"];
+        expect(localematch(["en-US", "en-GB"], available, "en-US")).toEqual(["en-US", "en-GB"]);
+        expect(localematch(["en-GB", "en-US"], available, "en-US")).toEqual(["en-GB", "en-US"]);
+        expect(localematch(["en-CA", "en-US"], available, "en-US")).toEqual(["en-GB", "en-US"]);
+        expect(localematch(["en-CA", "en-GB"], available, "en-US")).toEqual(["en-GB"]);
+    })
+
+    test('return_the_fallback_locale_if_no_match_is_found', () => {
+        expect(localematch(["am"], ["fr-FR", "de-DE"], "en-US")).toEqual(["en-US"]);
+    })
 })
